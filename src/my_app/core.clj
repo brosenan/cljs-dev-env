fdc60a20efdfc77e541e1bffb024d682b317adb8
Boaz Rosenan <brosenan@gmail.com>
2017-08-26 21:30:13 +0300

    front-end

--- src/my_app/core.clj
+++ src/my_app/core.clj
@@ -12,9 +12,9 @@
  ;; See http://axiom-clj.org/cloudlog.html for more information.
 
  ;; Rules work at update time.
- (clg/defrule task-where-user-is-mentioned [user author task]
+ (clg/defrule task-where-user-is-mentioned [user author task ts]
    ;; For every task created by an author,
-   [:my-app/task author task] (clg/by author)
+   [:my-app/task author task ts] (clg/by author)
    ;; For every pattern of the form @user in the text,
    (for [user (re-seq #"@[a-zA-Z0-9]+" task)])
    ;; Remove the first character to get the user name.
@@ -24,11 +24,11 @@
  ;; Each clause can create different kind of answer to the same question.
  ;; This clause answers the question of 'what are my tasks' with tasks I created myself.
  (clg/defclause tasks-i-wrote
-   [:my-app/my-tasks user -> user task]
-   [:my-app/task user task] (clg/by user))
+   [:my-app/my-tasks user -> user task ts]
+   [:my-app/task user task ts] (clg/by user))
 
  ;; This clause answers this question with tasks I was mentioned in
  (clg/defclause tasks-i-was-mentioned-in
-   [:my-app/my-tasks user -> author task]
-   [task-where-user-is-mentioned user author task]))
+   [:my-app/my-tasks user -> author task ts]
+   [task-where-user-is-mentioned user author task ts]))
 
--- test/my_app/core_test.clj
+++ test/my_app/core_test.clj
@@ -8,10 +8,11 @@
  ;; See http://axiom-clj.org/cloudlog-events.testing.html for more details.
  (scenario
   (as "alice"
-      (emit [:my-app/task "alice" "Create app"])
-      (emit [:my-app/task "alice" "Show app to @bob"])
-      (query [:my-app/my-tasks "alice"]) => #{["alice" "Create app"]
-                                              ["alice" "Show app to @bob"]})
-  (apply-rules [:my-app.core/task-where-user-is-mentioned "bob"]) => #{["alice" "Show app to @bob"]}
+      (emit [:my-app/task "alice" "Create app" 1000])
+      (emit [:my-app/task "alice" "Show app to @bob" 2000])
+      (query [:my-app/my-tasks "alice"]) => #{["alice" "Create app" 1000]
+                                              ["alice" "Show app to @bob" 2000]})
+  (apply-rules [:my-app.core/task-where-user-is-mentioned "bob"])
+  => #{["alice" "Show app to @bob" 2000]}
   (as "bob"
-      (query [:my-app/my-tasks "bob"]) => #{["alice" "Show app to @bob"]})))
+      (query [:my-app/my-tasks "bob"]) => #{["alice" "Show app to @bob" 2000]})))
